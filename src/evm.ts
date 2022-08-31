93b244fe067bc8b265051ee99a6defb21cbc7c48
vponline <vepant@gmail.com>
2022-08-31 12:28:37 +0300

    Add resolveChainId, resolveExplorerUrlByName to evm utils

--- src/evm.ts
+++ src/evm.ts
@@ -1,41 +1,63 @@
-import { parseUnits } from "ethers/lib/utils";
-import { OperationsRepository } from "@api3/operations";
-import { readOperationsRepository } from "@api3/operations/dist/utils/read-operations";
-import { EthValue } from "./types";
-import { log } from "./logging";
+import { parseUnits } from 'ethers/lib/utils';
+import { OperationsRepository } from '@api3/operations';
+import { readOperationsRepository } from '@api3/operations/dist/utils/read-operations';
+import { EthValue } from './types';
+import { log, logTrace } from './logging';
 
-export const doTimeout = (interval: number) =>
-  new Promise((resolve) => setTimeout(() => resolve(null), interval));
+export const doTimeout = (interval: number) => new Promise((resolve) => setTimeout(() => resolve(null), interval));
 
-export const convertEtherValue = (input: EthValue) =>
-  parseUnits(`${input.amount}`, input.units);
+export const convertEtherValue = (input: EthValue) => parseUnits(`${input.amount}`, input.units);
 
 export const exit = (code = 0) => {
   console.log(`Exiting, code: ${code}`);
   process.exit(code);
 };
 
-export const isCloudFunction = () =>
-  process.env.LAMBDA_TASK_ROOT || process.env.FUNCTION_TARGET;
+export const isCloudFunction = () => process.env.LAMBDA_TASK_ROOT || process.env.FUNCTION_TARGET;
 
-export const resolveChainName = async (
-  chainId: string,
-  operationsRepository?: OperationsRepository
-): Promise<string | null> => {
+export const resolveChainName = (chainId: string, operationsRepository?: OperationsRepository) => {
   const operations = operationsRepository ?? readOperationsRepository();
 
-  const chainName = Object.values(operations.chains).find(
-    (chain) => chain.id === chainId
-  )?.name;
+  const chainName = Object.values(operations.chains).find((chain) => chain.id === chainId)?.name;
 
   if (chainName) {
     return chainName;
   }
 
-  log("Invalid or unknown chain", "INFO", {
-    alias: "invalid-chain-id-resolveChainName",
+  log('Invalid or unknown chain', 'INFO', {
+    alias: 'invalid-chain-id-resolveChainName',
     description: `Please check the config and/or resolveChainName function: ${chainId}`,
-    priority: "P2",
+    priority: 'P2',
   });
   return null;
+};
+
+export const resolveChainId = (chainName: string, operationsRepository?: OperationsRepository) => {
+  const operations = operationsRepository ?? readOperationsRepository();
+
+  const chainId = Object.values(operations.chains).find((chain) => chain.name === chainName)?.id;
+
+  if (chainId) {
+    return chainId;
+  }
+
+  log('Invalid or unknown chain', 'INFO', {
+    message: 'Invalid or unknown chain',
+    alias: 'invalid-chain-name-resolveChainId',
+    description: `Please check the config and/or resolveChainId function: ${chainName}`,
+    priority: 'P2',
+  });
+  return null;
+};
+
+export const resolveExplorerUrlByName = async (explorerUrls: Record<string, string>, chainName: string) => {
+  const chainId = resolveChainId(chainName);
+
+  const explorerUrl = explorerUrls[chainId as string];
+
+  if (chainId && !explorerUrl) {
+    logTrace(`Unable to find explorer URL for chain: ${resolveChainName(chainId)}`);
+  }
+
+  return explorerUrl;
 };
