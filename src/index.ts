bb61d3ba181fb71da1d920e1c041db2a31b2cd4d
aquarat <blue@aquarat.co.za>
2022-09-16 15:32:31 +0200

    Introduce changes to support updating alerts and fix bug in caching

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * as promises from './promises';
 export * as logging from './logging';
 export * from './types';
-export * as opsGenie from './opsgenie';
+export * from './opsgenie';
 export * as evm from './evm';
 export * as jsonFiles from './json-files';
 export * as constants from './constants';
