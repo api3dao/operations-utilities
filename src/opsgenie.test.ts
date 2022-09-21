07f6b19ebcc276b77838be0ca5d8c9f7a54b8efc
aquarat <blue@aquarat.co.za>
2022-09-21 10:21:16 +0200

    Add test stub and CI flow

--- src/opsgenie.test.ts
+++ src/opsgenie.test.ts
@@ -1,8 +1,16 @@
 import { OpsGenieConfig } from './types';
+import { checkForOpsGenieApiKey } from './opsgenie';
 import { buildTelemetryConfig } from '../test/fixtures/config';
 
 const _opsGenieConfig = buildTelemetryConfig().opsGenieConfig as OpsGenieConfig;
 
 jest.mock('axios', () => jest.fn(() => Promise.resolve('teresa teng')));
 
-describe('alert utilities', () => {});
+describe('alert utilities', () => {
+  it('checks for an OpsGenie key - positive case', () => {
+    process.env.OPSGENIE_API_KEY = 'test key';
+
+    const result = checkForOpsGenieApiKey();
+    expect(result).toBeFalsy();
+  });
+});
